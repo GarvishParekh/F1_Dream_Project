2dd49b857809e4a9e77102176f516c43f30e092d
Garvish Parekh <garvish.p@theintellify.com>
2025-05-27 17:52:42 +0530

    Initial commit

---
+++ Assets/_My assets/Scripts/Controller/EngineController.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+
+public class EngineController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Rigidbody kartRb;
+
+    [Header("Scriptable")]
+    [SerializeField] private InputData inputData;
+    [SerializeField] private KartData kartData;
+
+    [Header("Wheel Colliders")]
+    [SerializeField] private WheelCollider frontLeftCollider;
+    [SerializeField] private WheelCollider frontRightCollider;
+    [SerializeField] private WheelCollider rearLeftCollider;
+    [SerializeField] private WheelCollider rearRightCollider;
+
+    [Header("Wheel Meshes")]
+    [SerializeField] private Transform frontLeftMesh;
+    [SerializeField] private Transform frontRightMesh;
+    [SerializeField] private Transform rearLeftMesh;
+    [SerializeField] private Transform rearRightMesh;
+
+    [Header("Car Settings")]
+    [SerializeField] private float maxMotorTorque = 1500f; // Adjust for high acceleration
+    [SerializeField] private float maxSteerAngle = 30f;
+    [SerializeField] private float brakeForce = 3000f;
+    [SerializeField] private float downForce = 100;
+
+    float motorInput;
+    float steerInput;
+    float brakeInput;
+
+    private void Awake()
+    {
+        kartRb.centerOfMass = new Vector3(0, -0.3f, 0);
+    }
+
+    void Update()
+    {
+        switch (inputData.controlType)
+        {
+            case ControlType.KEYBOARD:
+                motorInput = inputData.zKeybaord;
+                steerInput = inputData.xKeybaord;
+                brakeInput = Input.GetKey(KeyCode.Space) ? 1f : 0f; // Space for brake
+                break;
+            case ControlType.GYRO:
+                motorInput = inputData.zButton;
+                steerInput = inputData.xGyro;
+                break;
+        }
+        // Get player input
+    }
+
+    void FixedUpdate()
+    {
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        AppleDownForce();
+    }
+
+    private void HandleMotor()
+    {
+        // Apply motor torque to rear wheels (AWD)
+        rearLeftCollider.motorTorque = motorInput * maxMotorTorque;
+        rearRightCollider.motorTorque = motorInput * maxMotorTorque;
+        frontLeftCollider.motorTorque = motorInput * maxMotorTorque;
+        frontRightCollider.motorTorque = motorInput * maxMotorTorque;
+
+        // Apply brakes to all wheels
+        float appliedBrake = brakeInput * brakeForce;
+        frontLeftCollider.brakeTorque = appliedBrake;
+        frontRightCollider.brakeTorque = appliedBrake;
+        rearLeftCollider.brakeTorque = appliedBrake;
+        rearRightCollider.brakeTorque = appliedBrake;
+    }
+
+    private void HandleSteering()
+    {
+        float velocityMagnitude = kartRb.velocity.magnitude;
+        maxSteerAngle = Remap(velocityMagnitude, 0, 30, kartData.kartMaxTurn, kartData.kartMinTurn);
+        // Apply steer angle to front wheels only
+        float steer = steerInput * maxSteerAngle;
+        frontLeftCollider.steerAngle = steer;
+        frontRightCollider.steerAngle = steer;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateWheelPose(frontLeftCollider, frontLeftMesh);
+        UpdateWheelPose(frontRightCollider, frontRightMesh);
+        UpdateWheelPose(rearLeftCollider, rearLeftMesh);
+        UpdateWheelPose(rearRightCollider, rearRightMesh);
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform mesh)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        collider.GetWorldPose(out pos, out rot);
+        mesh.position = pos;
+        mesh.rotation = rot;
+    }
+
+    private void AppleDownForce()
+    {
+        float speed = kartRb.velocity.magnitude;
+        kartRb.AddForce(-transform.up * downForce * speed);
+    }
+
+    public float Remap(float value, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        float t = Mathf.InverseLerp(fromMin, fromMax, value); // Normalize to 0–1
+        return Mathf.Lerp(toMin, toMax, t); // Remap to new range
+    }
+}
---
+++ Assets/_My assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("<b>Scriptable")]
+    [SerializeField] private InputData inputData;
+    [SerializeField] private KartData kartData;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        switch (inputData.controlType)
+        {
+            case ControlType.GYRO: Input.gyro.enabled = true; break;
+        }
+    }
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnDisable()
+    {
+    }
+
+
+    private void Update()
+    {
+        switch (inputData.controlType)
+        {
+            case ControlType.KEYBOARD:
+                ReadKeyboardInputs();
+                break;
+            case ControlType.GYRO:
+                ReadGyro();
+                break;
+        }
+        CheckForRestart();
+    }
+
+    private void ReadKeyboardInputs()
+    {
+        inputData.zKeybaord = Input.GetAxis("Vertical");
+        inputData.xKeybaord = Input.GetAxis("Horizontal");
+    }
+
+    
+    private void CheckForRestart()
+    {
+        //if (Input.GetKeyDown(KeyCode.R)) ActionHandler.RestartScene?.Invoke();
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    float tiltValue = 0;
+    float sensitivity = 2;
+    private void ReadGyro()
+    {
+        float rawTilt = Input.acceleration.x;
+
+        // Clamp value between -1 and 1
+        tiltValue = Mathf.Clamp(rawTilt * sensitivity, -1f, 1f);
+        inputData.xGyro = tiltValue;
+    }
+
+    public void B_AccelerationDown() => inputData.zButton = 1;
+    public void B_AccelerationUp() => inputData.zButton = 0;
+    public void B_BreaksDown() => inputData.zButton = -1;
+    public void B_BreaksUp() => inputData.zButton = 0;
+}
